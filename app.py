2091d1352e02d8e3450f60b667b05ac07e8eb231
Shivam Saini <sainishivam319@gmail.com>
2024-03-13 00:37:13 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,url_for
+import requests
+from flask import request as req
+
+app=Flask(__name__)
+@app.route('/',methods=["GET","POST"])
+def Index():
+    return render_template("index.html")
+
+@app.route('/Summarize', methods = ["GET","POST"])
+
+def Summarize():
+    if req.method== "POST":
+         API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+         headers = {"Authorization": "Bearer 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"}
+
+         data=req.form["data"]
+        
+
+         minLen=20
+         maxLen=int(req.form["maxL"])
+
+         def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+            
+         output = query({
+            "inputs": data,
+            "parameters":{"min_length":minLen,"max_length":maxLen}
+        })[0]
+         return render_template("index.html",result=output["summary_text"])
+    else:
+        return render_template("index.html")
+
+
+if __name__=='__main__':
+    app.run(debug=True)
